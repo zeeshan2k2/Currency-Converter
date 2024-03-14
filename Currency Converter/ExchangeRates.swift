1a38a9ba4a92285d8ff06e908838449f3c8bdc84
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-03-15 00:35:48 +0500

    completed, just addin final details

---
+++ Currency Converter/ExchangeRates.swift
@@ -0,0 +1,12 @@
+//
+//  ExchangeRates.swift
+//  Currency Converter
+//
+//  Created by Zeeshan Waheed on 15/03/2024.
+//
+
+import Foundation
+
+struct ExchangeRates: Codable {
+    let rates: [String: Double]
+}
--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -7,13 +7,94 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+
+    
+    // IB Outlets (variables for all buttons)
+    
+    @IBOutlet var priceLabel: UILabel!
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var pickerView: UIPickerView!
+    
+    
+    // properties
+    
+    var currencyCode: [String] = []
+    var values: [Double] = []
+    var activeCurrency: Double = 0.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        fetchJSON()
+        
+        textField.addTarget(self, action: #selector(updateViews), for: .editingChanged)
+    }
+    
+    @objc func updateViews(input: Double) {
+        guard let amountText = textField.text, let theAmountText = Double(amountText) else { return }
+        if textField.text != "" {
+            let total = theAmountText * activeCurrency
+            priceLabel.text = String(format: "%.2f", total)
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
 
-
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyCode.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyCode[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        activeCurrency = values[row]
+        updateViews(input: activeCurrency)
+    }
+    
+//  method
+    
+    func fetchJSON() {
+        guard let url = URL(string: "https://open.er-api.com/v6/latest/USD") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            // handle any errors if present
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            // safetly unwrap the data
+            guard let safeData = data else { return }
+            
+            // decode the JSON Data
+            do {
+                let results = try JSONDecoder().decode(ExchangeRates.self, from: safeData)
+                self.currencyCode.append(contentsOf: results.rates.keys)
+                self.values.append(contentsOf: results.rates.values)
+                DispatchQueue.main.async {
+                    self.pickerView.reloadAllComponents()
+                }
+                // to check if we're getting the json data
+                print(results.rates)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+        
+    }
+    
 }
 
